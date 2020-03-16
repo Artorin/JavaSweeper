f0ceca49b81108d130509914e36fddd3a21f4d9f
artem_grishin <art.grischin@gmail.com>
2020-03-16 20:54:49 +0300

    0.2

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import sweeper.Box;
+import sweeper.Coord;
+import sweeper.Game;
+import sweeper.Ranges;
 
 public class JavaSweeper extends JFrame {
+
+    private Game game;
+
     private JPanel panel;
-    private final int COLS = 15;
-    private final int ROWS = 1;
+    private final int COLS = 9;
+    private final int ROWS = 9;
     private final int IMAGE_SIZE = 50;
 
     public static void main(String[] args) {
@@ -12,14 +19,27 @@ public class JavaSweeper extends JFrame {
         new JavaSweeper();
     }
     private JavaSweeper(){
-
+        game = new Game(COLS, ROWS);
+        setImages();
         initPanel();
         initFrame();
 
     }
     private void initPanel() {
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(COLS*IMAGE_SIZE, ROWS*IMAGE_SIZE));
+        panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                for (Coord coord: Ranges.getAllCoords()) {
+
+                    g.drawImage((Image) game.getBox(coord).image,
+                            coord.x * IMAGE_SIZE, coord.y * IMAGE_SIZE, this);
+                    
+                }
+
+            }
+        };
+        panel.setPreferredSize(new Dimension(Ranges.getSize().x* IMAGE_SIZE, Ranges.getSize().y*IMAGE_SIZE));
         add(panel);
     }
     private void initFrame() {
@@ -29,6 +49,17 @@ public class JavaSweeper extends JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
+        setIconImage(getImage("icon"));
+    }
+    private void setImages() {
+        for (Box box: Box.values())
+            box.image = getImage(box.name().toLowerCase());
+    }
+
+    private Image getImage (String name) {
+        String filename = "img/" + name.toLowerCase() + ".png/";
+        ImageIcon icon = new ImageIcon(getClass().getResource(filename));
+        return icon.getImage();
     }
 
 }
