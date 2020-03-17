b3f407a47c7d465b4c40a789e0937ed3c14b5ae0
artem_grishin <art.grischin@gmail.com>
2020-03-17 17:47:42 +0300

    0.3

--- src/sweeper/Bomb.java
+++ src/sweeper/Bomb.java
@@ -6,15 +6,40 @@ package sweeper;
 
      public Bomb(int totalBombs) {
          this.totalBombs = totalBombs;
+         fixBombsCount();
      }
      void start () {
          bombMap = new Matrix(Box.ZERO);
+         for (int i = 0; i < totalBombs; i++) {
+             placeBomb();
+             
+         }
          placeBomb();
      }
-     private void placeBomb() {
-         bombMap.set(new Coord(4,4),Box.BOMB);
+
+     private void fixBombsCount () {
+         int maxBombs = Ranges.getSize().x*Ranges.getSize().y/2;
+         if (totalBombs > maxBombs)
+             totalBombs = maxBombs;
      }
+
+     private void placeBomb() {
+         while (true) {
+         Coord coord = Ranges.getRandomCoord();
+         if (Box.BOMB == bombMap.get(coord))
+                     continue;
+         bombMap.set(coord, Box.BOMB);
+         incNumbersAroundBomb(coord);
+         break;
+
+     }}
      Box get(Coord coord) {
          return bombMap.get(coord);
+     }
+
+     private void incNumbersAroundBomb (Coord coord) {
+         for (Coord around: Ranges.getCoordsAround(coord))
+             if (Box.BOMB != bombMap.get(around))
+             bombMap.set(around, bombMap.get(around).getNextNumberBox());
      }
  }
